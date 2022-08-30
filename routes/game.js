9f78430601d297c26aa059fe7399e93165b4001c
DDRAGON <davide.0829@gmail.com>
2022-08-30 21:52:32 +0900

    別の箇所にもあった config のパスを修正

--- routes/game.js
+++ routes/game.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const config = require('../config');
+const config = require('/etc/secrets/config');
 
 router.get('/', (req, res, next) => {
   let displayName = 'anonymous';
